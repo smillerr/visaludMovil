ee0947598151e09459a1102c622d751e7d8ab9ba
CARLOS MARIO RESTREPO <cmrestrepoc@unal.edu.co>
2021-05-02 18:01:23 -0500

    bundle analyzer added

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,6 +6,7 @@ const CopyPlugin = require('copy-webpack-plugin')
 /* const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin') */
 const Dotenv = require('dotenv-webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 
 module.exports = {
     entry: './src/index.js',
@@ -78,7 +79,8 @@ module.exports = {
                 }
             ]
         }),
-        new Dotenv()
+        new Dotenv(),
+        new BundleAnalyzerPlugin()
     ],
     /** optimization is not required in development */
     /* optimization: {
