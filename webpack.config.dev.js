749e2378ccc2eef0229c5f37d94fb804f2849185
CARLOS MARIO RESTREPO <cmrestrepoc@unal.edu.co>
2021-05-02 17:28:46 -0500

    watch mode added from webpack fil config in dev mode and from package json for production mode

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,6 +15,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
+    watch: true, // watches changes and recompiles
     resolve: {
         extensions: ['.js'],
         alias: {
